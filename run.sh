aea1a6e64fa46beaf86874a93da8cc5450b1e99e
Tom Moya Schiller <moya@asofterspace.com>
2019-11-10 19:23:32 +0100

    Run with more heap space

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cd `dirname "$0"`
+# check if we are running a 64-Bit version of Java
+javaversion=$(java -version 2>&1)
 
-java -classpath "`dirname "$0"`/bin" -Xms16m -Xmx1024m com.asofterspace.picturizer.Main "$@"
+if [[ $javaversion = *"64-Bit"* ]]; then
+	# we are running 64 bit - nice! assign loads of heap!
+	java -classpath "`dirname "$0"`/bin" -Xms16m -Xmx4096m com.asofterspace.picturizer.Main "$@"
+else
+	# we are running 32 bit - oh no! assign only a tinly little bit of heap!
+	java -classpath "`dirname "$0"`/bin" -Xms16m -Xmx1024m com.asofterspace.picturizer.Main "$@"
+fi
