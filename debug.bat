08a8cefa93a9d720428120fa70a36801e6943387
Tom Moya Schiller <moya@asofterspace.com>
2019-11-10 23:43:56 +0100

    Improve debug scripts

--- debug.bat
+++ debug.bat
@@ -1,6 +1,17 @@
 @echo off
 
-REM cd /D %~dp0
+REM   check if we are running a 64-Bit version of Java
+java -version 2>&1 | find "64-Bit" >nul
 
-java -classpath "%~dp0\bin" -Xms16m -Xmx1024m com.asofterspace.picturizer.Main %*
-pause
+if errorlevel 1 goto :32bit
+
+:64bit
+REM   we are running 64 bit - nice! assign loads of heap!
+start "Picturizer" java -classpath "%~dp0\bin" -Xms16m -Xmx4096m com.asofterspace.picturizer.Main %*
+goto :end
+
+:32bit
+REM   we are running 32 bit - oh no! assign only a tinly little bit of heap!
+start "Picturizer" java -classpath "%~dp0\bin" -Xms16m -Xmx1024m com.asofterspace.picturizer.Main %*
+
+:end
