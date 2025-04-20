de1592794d27ec78e2820d37935affa73ba379fc
Moya <moya@asofterspace.com>
2025-04-20 12:01:26 +0200

    Start without blocking the console by default

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ javaversion=$(java -version 2>&1)
 
 if [[ $javaversion = *"64-Bit"* ]]; then
 	# we are running 64 bit - nice! assign loads of heap!
-	java -classpath "`dirname "$0"`/bin" -Xms16m -Xmx4096m com.asofterspace.picturizer.Main "$@"
+	java -classpath "`dirname "$0"`/bin" -Xms16m -Xmx4096m com.asofterspace.picturizer.Main "$@" &
 else
 	# we are running 32 bit - oh no! assign only a tinly little bit of heap!
-	java -classpath "`dirname "$0"`/bin" -Xms16m -Xmx1024m com.asofterspace.picturizer.Main "$@"
+	java -classpath "`dirname "$0"`/bin" -Xms16m -Xmx1024m com.asofterspace.picturizer.Main "$@" &
 fi
